608ea24b582686b879a05043f425576f49a968f4
Hyeman Samuel <hyeman1738@gmail.com>
2022-06-03 20:25:49 +0100

    changed datasource entity property

--- src/utility/persistence/data-source.ts
+++ src/utility/persistence/data-source.ts
@@ -1,8 +1,17 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from "dotenv";
+import { CategoryEntity } from '../../modules/category/entities/category.entity';
+import { FrameEntity } from '../../modules/frame/entities/frame.entity';
+import { OrderEntity } from '../../modules/order/entities/order.entity';
+import { PackageEntity } from '../../modules/package/entities/package.entity';
+import { PlatformEntity } from '../../modules/platform/entities/platform.entity';
+import { PackageToCategory } from '../../modules/package/category_package/entities/category_package.entity';
+import { PackageToPlatform } from '../../modules/platform/platform_package/entities/platform_package.entity';
+import { UserEntity } from '../../modules/auth/entities/user.entity';
+import { OrderDetailEntity } from '../../modules/order/orderdetails/entities/orderdetail.entity';
 dotenv.config();
 
-const entitiesDir = (process.env.NODE_ENV != "development")?"src/**/*.entity{.js}":"src/**/*.entity{.ts}"
+//const entitiesDir = (process.env.NODE_ENV != "development")?"src/**/*.entity{.js}":"src/**/*.entity{.ts}"
 
 const migrationsDir = (process.env.NODE_ENV != "development")?"src/migrations/**{.js}":"src/migrations/**{.ts}"
 
@@ -18,7 +27,15 @@ const migrationsDir = (process.env.NODE_ENV != "development")?"src/migrations/**
             synchronize: false,
             logging: true,
             entities: [
-                entitiesDir
+                CategoryEntity,
+                FrameEntity,
+                OrderEntity,
+                OrderDetailEntity,
+                PackageEntity,
+                PackageToCategory,
+                PackageToPlatform,
+                PlatformEntity,
+                UserEntity
             ],
             migrations:[
                 migrationsDir
